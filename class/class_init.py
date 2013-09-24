afa98fdc7123c16719691056ca9b628cc7693b7a
zjw0722 <zjw_0722@163.com>
2013-09-25 02:15:33 +0800

    class

---
+++ class/class_init.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python
+# Filename:class_init.py
+
+class Person:
+	def __init__(self, name):
+		self.name = name
+	def sayHi(self):
+		print 'Hello, my name is', self.name
+
+p = Person("zjw")
+p.sayHi()
+
+# This short example can also be written as Person('zjw').sayHi()
